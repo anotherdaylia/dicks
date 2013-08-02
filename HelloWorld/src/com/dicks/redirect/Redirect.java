6e3c4d9def754d86be45e4a88b7df2ef2a4395ab
edhuacao <edhuacao@gmail.com>
2013-08-01 22:28:44 -0400

    Signed-off-by: edhuacao <edhuacao@gmail.com>

---
+++ HelloWorld/src/com/dicks/redirect/Redirect.java
@@ -0,0 +1,13 @@
+package com.dicks.redirect;
+
+public class Redirect {
+	public String gotorulelist(){
+		return "gotorulelist";
+	}
+	public String gotoorderlist(){
+		return "gotoorderlist";
+	}
+	public String gotocategorylist(){
+		return "gotocategorylist";
+	}
+}
