0e3a0dca652f2085e6b5ed630310ad22eddb3c48
miqicheng <miqicheng1999@gmail.com>
2013-08-04 17:35:10 -0400

    micky

--- HelloWorld/src/com/dicks/pojo/Rule.java
+++ HelloWorld/src/com/dicks/pojo/Rule.java
@@ -1,38 +1,124 @@
 package com.dicks.pojo;
 
+import java.util.Set;
+
 public class Rule {
 	private String ruleName;
-	private String ruleID;
-	private String ruleDRL;
+	private int ruleID;
+	private String ruleURL;
 	private String ruleDescr;
 	private int priority;
 	private char flag; 
 	private char type;
+	private char stage;
+	private String object;
+	private String attribute;
+	private String operator;
+	private String value;
+	private String route;
+	private String action;	
+	private Set<RuleCategory> ruleCategorys;
 	
 	
 	
-	
-	
-	
-	
+	public Set<RuleCategory> getRuleCategorys() {
+		return ruleCategorys;
+	}
+	public void setRuleCategorys(Set<RuleCategory> ruleCategorys) {
+		this.ruleCategorys = ruleCategorys;
+	}
+	public Rule(String ruleName,  String ruleURL,
+			String ruleDescr, int priority, char flag, char type, char stage,
+			String object, String attribute, String operator, String value,
+			String route, String action) {
+		super();
+		this.ruleName = ruleName;
+		this.ruleURL = ruleURL;
+		this.ruleDescr = ruleDescr;
+		this.priority = priority;
+		this.flag = flag;
+		this.type = type;
+		this.stage = stage;
+		this.object = object;
+		this.attribute = attribute;
+		this.operator = operator;
+		this.value = value;
+		this.route = route;
+		this.action = action;
+	}
+	public String getRuleURL() {
+		return ruleURL;
+	}
+	public void setRuleURL(String ruleURL) {
+		this.ruleURL = ruleURL;
+	}
+	public char getFlag() {
+		return flag;
+	}
+	public void setFlag(char flag) {
+		this.flag = flag;
+	}
+	public char getType() {
+		return type;
+	}
+	public void setType(char type) {
+		this.type = type;
+	}
+	public char getStage() {
+		return stage;
+	}
+	public void setStage(char stage) {
+		this.stage = stage;
+	}
+	public String getObject() {
+		return object;
+	}
+	public void setObject(String object) {
+		this.object = object;
+	}
+	public String getAttribute() {
+		return attribute;
+	}
+	public void setAttribute(String attribute) {
+		this.attribute = attribute;
+	}
+	public String getOperator() {
+		return operator;
+	}
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getRoute() {
+		return route;
+	}
+	public void setRoute(String route) {
+		this.route = route;
+	}
+	public String getAction() {
+		return action;
+	}
+	public void setAction(String action) {
+		this.action = action;
+	}
 	public String getRuleName() {
 		return ruleName;
 	}
 	public void setRuleName(String ruleName) {
 		this.ruleName = ruleName;
 	}
-	public String getRuleID() {
+	public int getRuleID() {
 		return ruleID;
 	}
-	public void setRuleID(String ruleID) {
+	public void setRuleID(int ruleID) {
 		this.ruleID = ruleID;
 	}
-	public String getRuleDRL() {
-		return ruleDRL;
-	}
-	public void setRuleDRL(String ruleDRL) {
-		this.ruleDRL = ruleDRL;
-	}
+
 	public String getRuleDescr() {
 		return ruleDescr;
 	}
